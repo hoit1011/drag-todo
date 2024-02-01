4cf0419caef6c8ae2f420e66585b96ae47199933
hoit1011 <parksm071011@gmail.com>
2024-02-02 02:02:58 +0900

    first-commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import './App.css'
+
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+
+function App() {
+
+  return (
+    <div className='container'>
+      <div className='weeklist'>
+        <div>asdf</div>
+      </div>
+      <div className='todaylist'>
+        <div>asdf</div>
+      </div>
+      <div className='donelist'>
+        <div>asdf</div>
+      </div>
+    </div>
+  )
+}
+
+export default App
