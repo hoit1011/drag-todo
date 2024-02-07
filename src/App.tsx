2bfc4234c6bf14d2b3d272668452c263a1654182
hoit1011 <parksm071011@gmail.com>
2024-02-07 23:15:51 +0900

    addbox

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 
-import { DndProvider } from 'react-dnd';
+import { useDrag, useDrop,DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 
 function App() {
+  const [boxes, setboxes] = useState<{id : number; text: string;}[]>([])
+
+  const handleAddBox = () => {
+    const newBox = { id: boxes.length + 1, text: `박스 ${boxes.length + 1}`}
+    console.log("아아")
+    setboxes([...boxes, newBox])
+  }
 
   return (
+    <DndProvider backend={HTML5Backend}>
     <div>
       <div className='listcontainer'>
         <div className='weeklist'>
           <div className='title'>이번주 해야할 일</div>
-            <div className='addlist'>추가</div>
+            <div className='addlist' onClick={handleAddBox}>추가</div>
+            
         </div>
         <div className='todaylist'>
           <div className='title'>오늘 해야할 일</div>
@@ -21,6 +30,7 @@ function App() {
         </div>
       </div>
     </div>
+    </DndProvider>
   )
 }
 
