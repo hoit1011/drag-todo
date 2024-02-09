f4b418ba55ae55b77148dd292fa2741540522ae6
hoit1011 <parksm071011@gmail.com>
2024-02-10 04:30:10 +0900

    smooth dnd

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
 import { DragDropContext, Draggable, Droppable, OnDragEndResponder } from "react-beautiful-dnd"
+import "./App.css"
 
 export default function App() {
 
-  const [todos, setTodos] = useState([
+  const [weekTodos, setWeekTodos] = useState([
     { id: "1", title: "공부" },
     { id: "2", title: "헬스" },
     { id: "3", title: "독서" },
@@ -11,43 +12,54 @@ export default function App() {
     { id: "5", title: "요리" }
   ]);
 
-  const handleChange: OnDragEndResponder = (result) => {
+  const handleWeekChange: OnDragEndResponder = (result) => {
     if (!result.destination) return;
-    console.log(result);
-    const items = [...todos];
+    const items = [...weekTodos];
     const [reorderedItem] = items.splice(result.source.index, 1);
     items.splice(result.destination.index, 0, reorderedItem);
 
-    setTodos(items);
+    setWeekTodos(items);
   };
 
   return (
-    <DragDropContext onDragEnd={handleChange}>
-      <Droppable droppableId="todos">
-        {(provided) => (
-          <ul
-            className="todos"
-            {...provided.droppableProps}
-            ref={provided.innerRef}
-          >
-            {todos.map(({ id, title }, index) => (
-              <Draggable key={id} draggableId={id} index={index}>
-                {(provided) => (
-                  <li
-                    key={index}
-                    ref={provided.innerRef}
-                    {...provided.dragHandleProps}
-                    {...provided.draggableProps}
-                  >
-                    {title}
-                  </li>
-                )}
-              </Draggable>
-            ))}
-            {provided.placeholder}
-          </ul>
-        )}
-      </Droppable>
+    <DragDropContext onDragEnd={handleWeekChange}>
+      <div className="listcontainer">
+        <div className="weeklist">
+          <div className="title">이번주 해야할일</div>
+          <Droppable droppableId="weekTodos">
+            {(provided) => (
+              <div
+                className="todos"
+                {...provided.droppableProps}
+                ref={provided.innerRef}
+              >
+                {weekTodos.map(({ id, title }, index) => (
+                  <Draggable key={id} draggableId={id} index={index}>
+                    {(provided) => (
+                      <div
+                        key={index}
+                        ref={provided.innerRef}
+                        {...provided.dragHandleProps}
+                        {...provided.draggableProps}
+                      >
+                        {title}
+                      </div>
+                    )}
+                  </Draggable>
+                ))}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+        </div>
+        <div className="todaylist">
+          <div className="title">오늘 해야할 일</div>
+        </div>
+        <div className="donelist">
+          <div className="title">완료한 일</div>  
+        </div>
+      </div>
     </DragDropContext>
   )
 }
+                 
